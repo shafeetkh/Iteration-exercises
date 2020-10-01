6b4af2579b8ec4916c89e011cabd3d5e601c1a26
Shafee <shafeelinks@gmail.com>
2020-09-30 21:10:06 -0400

    completed, with Zipporah and Tinesha

--- main.js
+++ main.js
@@ -2,10 +2,34 @@
 //Using map, write a function that takes an array of numbers
 //and makes them strings
 
+// let numbers = [1, 2, 3, 4, 5];
+// function convertToStrings(arr) {
+//     let arrOfStrings = arr.map(String);
+//     return arrOfStrings;
+// }
+// console.log(convertToStrings(numbers));
+
+const intoStr = (arr) => arr.map((val) => `${val}`);
+
+intoStr([1, 2, 3, 4, 5]);
+
 //Exercise #2
 //You must use forEach for this exercise
 //Write a function that accepts an array and returns a new array
 //with all the values in the array passed to the function doubled
+
+function doubled(arr) {
+    let newArr = [];
+
+    arr.forEach(index => {
+        let double = index * 2;
+        newArr.push(double);
+    })
+    return newArr;
+}
+
+doubled([1, 2, 3, 4]);
+
 
 //Exercise #3
 //Write a function called firstAndLast which accepts
@@ -14,6 +38,28 @@
 //Example: ['winning', 'tomato'] => ['wg', 'to']
 //hint: forEach show be used here
 
+function firstAndLast(arr) {
+    let newArr = [];
+
+    arr.forEach(index => {
+        let firstIndex = index.charAt(0);
+        let lastIndex = index.charAt(index.length - 1);
+
+        newArr.push(`${firstIndex}${lastIndex}`)
+    })
+    return newArr;
+}
+
+firstAndLast(['Zipporah', 'Tinesha', 'Shafee']);
+
 //Bonus:
 //Write a for loop that outputs this:
 // 1 1 1 2 2 2 3 3 3 4 4 4
+
+let arr = [];
+
+for (i = 1; i <= 4; i++) {
+    arr.push(`${i} ${i} ${i}`);
+}
+
+console.log(arr.join(" "));
